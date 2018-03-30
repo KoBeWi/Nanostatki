de20174f61f9693ce3ddcab1b1ba96a03d1c0517
KoBeWi <kobewi4e@gmail.com>
2018-03-31 00:50:21 +0200

    Pole magnetyczne

--- Scripts/Magnetic Field.gd
+++ Scripts/Magnetic Field.gd
@@ -1,19 +1,18 @@
 extends Area2D
 
-onready var tex1 = load("res://Sprites/Magnet.png")
-onready var tex2 = load("res://Sprites/Magnet2.png")
-onready var size = $Field.shape.extents * 2
-onready var players = $"../Players".get_children()
+onready var tex1 = load("res://Sprites/Common/MagnetDot.png")
+onready var tex2 = load("res://Sprites/Common/MagnetCross.png")
 
 const FORCE = PI/40
-const DDIST = 25
+const DDIST = 50
+
+onready var size = $Field.shape.extents * 2
+onready var cols = int(size.x / DDIST) + 2
+onready var rows = int(size.y / DDIST) + 2
 
 export var charge = 1
 
 var players_in = []
-
-func _ready():
-	pass
 
 func _physics_process(delta):
 	for player in players_in:
@@ -23,14 +22,11 @@ func _physics_process(delta):
 	update()
 
 func _draw():
-	var cols = size.x / DDIST
-	var rows = size.y / DDIST
-	
 	var color = Color(1, 1, 1, abs(sin(OS.get_ticks_msec() / 250.0)) + 0.5)
 	var texture = [tex1, tex2][(2 - charge)/2]
 	for x in range(cols):
 		for y in range(rows):
-			draw_texture(texture, Vector2(DDIST/2 - size.x/2 + x * DDIST, DDIST/2 - size.y/2 + y * DDIST), color)
+			draw_texture(texture, Vector2(-DDIST/2 - size.x/2 + x * DDIST, -DDIST/2 - size.y/2 + y * DDIST), color)
 
 func _on_enter(body):
 	if body.is_in_group("players"):
