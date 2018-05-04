90edfe7d2da7ba903dcf3a069215b32ddc1c9e83
KoBeWi <kobewi4e@gmail.com>
2018-05-05 01:32:34 +0200

    imiony

--- Scripts/Summary.gd
+++ Scripts/Summary.gd
@@ -23,34 +23,53 @@ func setup(_players, _scores, scoreboard):
 			spots[i] = Com.add_score("", _scores[i])
 			
 			for j in range(i):
-				if spots[i] < spots[j]: spots[j] += 1
+				if spots[i] <= spots[j]: spots[j] += 1
 	
 	for i in range(4):
 		var spot = get_node("Player" + str(i+1))
-		if spots[i] < CAN_NAME[mode]: name_queue.append(i)
 		
 		if _players[i] > -1:
+			if spots[i] < CAN_NAME[mode]: name_queue.append(i)
+			
 			match mode:
 				"Race":
 					spot.get_node("Score").text = " Czas: " + Com.format_time(-_scores[i])
 					spot.get_node("Rank").text = "Miejsce w rankingu: " + str(spots[i]+1)
 		else:
 			spot.visible = false
+	
+	set_player()
 
 func _process(delta):
-	$EnterName/NameInput.grab_focus()
-	$EnterName/TheName.text = $EnterName/NameInput.text + ("" if OS.get_ticks_msec() % 1000 < 500 else "|")
+	if !name_queue.empty():
+		$EnterName/NameInput.grab_focus()
+		$EnterName/TheName.text = $EnterName/NameInput.text + ("" if OS.get_ticks_msec() % 1000 < 500 else "|")
+		
+		if Input.is_key_pressed(KEY_ENTER) and $EnterName/NameInput.text != "":
+			names[name_queue.front()] = $EnterName/NameInput.text
+			name_queue.pop_front()
+			set_player()
+	else:
+		$EnterName.visible = false
+		$RealAnyKey.visible = true
 	
-	if Input.is_action_just_pressed("ui_accept"):
-		for i in range(4):
-			if players[i] > -1:
-				Com.update_name(spots[i], names[i])
-		
-		Com.save_scoreboard()
-		
-		var menu = load("res://Scenes/MainMenu.tscn").instance()
-		$"/root".add_child(menu)
-		menu.goto_lobby(Com.MODES.find(mode))
-		menu.get_node("Camera").current = true
-		get_tree().current_scene = menu
-		queue_free()
+		if Input.is_action_just_pressed("ui_accept"):
+			for i in range(4):
+				if players[i] > -1:
+					Com.update_name(spots[i], names[i])
+			
+			Com.save_scoreboard()
+			
+			var menu = load("res://Scenes/MainMenu.tscn").instance()
+			$"/root".add_child(menu)
+			menu.goto_lobby(Com.MODES.find(mode))
+			menu.get_node("Camera").current = true
+			get_tree().current_scene = menu
+			queue_free()
+
+func set_player():
+	if name_queue.empty(): return
+	
+	var i = name_queue.front()
+	$EnterName.modulate = Com.PLAYER_COLORS[i]
+	$EnterName/NameInput.text = ""
